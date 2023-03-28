b68b708d0c31de7ca84305d7242d26aece525e5b
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2023-03-27 21:38:03 -0700

    Hello, Tokio

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    client.set("hello", "world".into()).await?;
+
+    let result = client.get("hello").await?;
+
+    if let Some(val) = result {
+        println!("hello: {:?}", val);
+    }
+
+    return Ok(());
+}
