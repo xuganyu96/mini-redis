f832c69e9ec32f841d981ab4aac86b3dfb73fd89
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2023-04-03 21:59:56 -0700

    Sharded database

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -7,14 +7,59 @@ use tokio;
 use tokio::net::{ TcpListener, TcpStream };
 use mini_redis::{ Connection, Frame, Command };
 use std::sync::{ Arc, Mutex };
+use std::hash::{ Hash, Hasher };
+use std::collections::hash_map::DefaultHasher;
 use bytes::Bytes;
 
 type AsyncHashMap<T, U> = Arc<Mutex<HashMap<T, U>>>;
 
+type Shard<T, U> = Mutex<HashMap<T, U>>;
+struct ShardedHashMap<T, U> {
+    shards: Vec<Shard<T, U>>,
+}
+
+impl<T: Hash + Eq, U: Clone> ShardedHashMap<T, U> {
+    fn new(n: usize) -> Self {
+        let mut shards = vec![];
+        for _ in 0..n {
+            shards.push(Mutex::new(HashMap::new()));
+        }
+
+        return Self { shards };
+    }
+
+    fn get(&self, key: &T) -> Option<U> {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        let shard = self.shards.get(hasher.finish() as usize % self.shards.len());
+        if let Some(shard) = shard {
+            let lock = shard.lock().unwrap();
+            let val = lock.get(key);
+            if let None = val {
+                return None;
+            }
+            if let Some(val) = val {
+                return Some(val.clone());
+            }
+        }
+        return None;
+    }
+
+    fn set(&self, key: T, val: U) {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        let shard = self.shards.get(hasher.finish() as usize % self.shards.len());
+        if let Some(shard) = shard {
+            let mut lock = shard.lock().unwrap();
+            lock.insert(key, val);
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    let db: AsyncHashMap<String, Bytes> = Arc::new(Mutex::new(HashMap::new()));
+    let db: Arc<ShardedHashMap<String, Bytes>> = Arc::new(ShardedHashMap::new(10));
 
     while let Ok((stream, _)) = listener.accept().await {
         let db = db.clone();
@@ -24,18 +69,17 @@ async fn main() {
     }
 }
 
-async fn process(stream: TcpStream, db: AsyncHashMap<String, Bytes>) {
+async fn process(stream: TcpStream, db: Arc<ShardedHashMap<String, Bytes>>) {
     let mut connection = Connection::new(stream);
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let cmd = Command::from_frame(frame).unwrap();
         let response = match cmd {
             Command::Set(cmd) => {
-                db.lock().unwrap()
-                    .insert(cmd.key().to_string(), cmd.value().clone());
+                db.set(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             },
             Command::Get(cmd) => {
-                match db.lock().unwrap().get(cmd.key()) {
+                match db.get(&cmd.key().to_string()) {
                     Some(val) => Frame::Bulk(val.clone().into()),
                     None => Frame::Null,
                 }
