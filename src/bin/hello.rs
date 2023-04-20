b8be93b691e19eb1abdb19ff5356a62a33fff665
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2023-04-19 22:23:17 -0700

    More skeletal code

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -1,10 +1,21 @@
-async fn say_world() {
-    println!("world!");
-}
+use tokio::net::{ TcpListener, TcpStream };
+use tokio::io::{ AsyncReadExt, AsyncWriteExt };
 
 #[tokio::main]
 async fn main() {
-    let handle = say_world();
-    print!("Hello, ");
-    handle.await;
+    let listener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+
+    if socket.writable().await.is_ok() {
+        let buffer = "那么古尔丹，代价是什么呢\n".to_string();
+        socket.write_all(buffer.as_bytes()).await.unwrap();
+    }
+
+    if socket.readable().await.is_ok() {
+        let mut buffer: Vec<u8> = vec![0;24];
+        socket.read(&mut buffer).await.unwrap();
+
+        let msg = String::from_utf8(buffer).unwrap();
+        println!("{msg}");
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,134 @@
-//! Underlying components of mini Redis
-use tokio::io::{ self, AsyncReadExt };
-use tokio::net::TcpStream;
-use mini_redis::{ self, Frame };
+//! Shared layers of abstraction: Bytes, Frame, Command, Connection, Client
+use std::error::Error;
+use tokio::net::{ TcpListener, TcpStream };
 
-struct Connection {
-    socket: TcpStream,
-    buffer: Vec<u8>,
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+/// A client provides high-level methods for sending commands to and receiving
+/// commands from the server.
+pub struct Client {
+    listener: TcpListener,
+    // ... other states ...
+}
+
+impl Client {
+    /// Connect to the server specified at the input address, or return any
+    /// error while attempting the connect
+    pub async fn connect(addr: &str) -> MyResult<Self> {
+        let listener = TcpListener::bind(addr).await?;
+        return Ok(Self { listener });
+    }
+
+    /// Send a "SET key val" command to the server. Return the Ok variant if
+    /// the server returns a OK.
+    pub async fn set(&mut self, key: String, val: String) -> MyResult<()> {
+        todo!();
+    }
+
+    /// Send a "GET key" command to the server. Return the Ok variant if the
+    /// server returns some valid results.
+    ///
+    /// If the key has a match in the server, then the associated value is
+    /// returned in the "Some" variant. If the key has no match, then the
+    /// "None" variant is returned.
+    pub async fn get(&mut self, key: String) -> MyResult<Option<String>> {
+        todo!();
+    }
+
+    /// Send a "POP key" command to the server. Return the Ok variant if the
+    /// server returns some valid results.
+    ///
+    /// If the key has a match in the server and the key is successfully
+    /// deleted, then the associated value is returned. If the key has no
+    /// match, or other things went wrong during the deletion, then the
+    /// error message is returned.
+    pub async fn pop(&mut self, key: String) -> MyResult<Result<String, String>> {
+        todo!();
+    }
+}
+
+/// The Command enum provides abstraction over Frames
+enum Command {
+    Set{ key: String, val: String },
+    Get{ key: String, },
+    Pop{ key: String, },
+}
+
+impl Command {
+    /// Create a new Set command
+    fn set(key: String, val: String) -> Self {
+        return Self::Set{ key, val };
+    }
+
+    /// Create a new Get command
+    fn get(key: String) -> Self {
+        return Self::Get{ key };
+    }
+
+    /// Create a new Pop command
+    fn pop(key: String) -> Self {
+        return Self::Pop{ key };
+    }
+
+    /// Convert a command into the appropriate Frame
+    fn to_frame(&self) -> Frame {
+        todo!();
+    }
+}
+
+/// The various RESP data types
+enum Frame {
+    Simple(String),
+    Error(String),
+    Integer(u64),
+    Bulk(Vec<u8>),
+    Array(Vec<Frame>),
+}
+
+impl Frame {
+    /// Serialize a frame into a byte array for transmission over the network
+    fn as_bytes(&self) -> Bytes {
+        todo!();
+    }
+
+    /// Read the input byte array and check if there is a valid frame inside.
+    /// If yes, return the parsed frame in the "Some" variant, else return
+    /// None
+    fn parse(bytes: &Bytes) -> Option<Frame> {
+        todo!();
+    }
+}
+
+/// A wrapper around a byte array with a cursor
+struct Bytes {
+    bytes: Vec<u8>,
     cursor: usize,
 }
 
+/// A wrapper around a TCP socket (TcpStream) for writing byte stream into
+/// Bytes and for parsing Bytes into frames
+struct Connection {
+    socket: TcpStream,
+}
+
 impl Connection {
-    /// Initialize the redis connection including an empty buffer and the 
-    /// cursor. Page size is hard-coded and hidden from the caller
+    /// Instantiate a new connection
     fn new(socket: TcpStream) -> Self {
-        return Self {
-            socket,
-            buffer: vec![0; 4096],
-            cursor: 0,
-        };
+        return Self { socket };
     }
 
-    /// Try to read a single frame from the underlying TCP stream. Return
-    /// the frame if there is a complete, otherwise return an empty frame or
-    /// report error
-    async fn read_frame(&mut self) -> mini_redis::Result<Option<Frame>> {
-        loop {
-            if let Some(frame) = self.parse_frame() {
-                return Ok(Some(frame));
-            }
-
-            // try to read more data, but before we can read more data, need to
-            // check whether we have buffer space left:
-            if self.cursor == self.buffer.len() {
-                self.buffer.resize(self.cursor * 2, 0);
-            }
-
-            match self.socket.read(&mut self.buffer[self.cursor..]).await? {
-                0 if self.cursor == 0 => {
-                    return Ok(None);  // no frame is read
-                },
-                0 if self.cursor > 0 => {
-                    eprintln!("Connection reset unexpectedly");
-                    return Err("Connection reset unexpectedly".into());
-                },
-                n => {
-                    self.cursor += n;
-                }
-            }
-        }
-    }
-
-    async fn write_frame(&mut self, frame: Frame) -> io::Result<()> {
+    /// Read bytes from the TcpStream, then parse it. If there is a valid
+    /// Frame in the bytes read, then return it. Else return None.
+    fn read_frame() -> Option<Frame> {
         todo!();
     }
 
-    /// Check if the internal buffer constitutes a complete frame. If yes,
-    /// return the frame and clear the buffer. If no, return None
-    fn parse_frame(&self) -> Option<Frame> {
+    /// Convert the input frame into bytes, then write into the socket
+    fn write_frame() {
         todo!();
+    }
+
+    /// Convert the command into the correct Frame, then pass into write_frame
+    fn emit_command(&mut self, cmd: Command) {
     }
 }
