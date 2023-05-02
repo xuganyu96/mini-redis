bb31a3603223c394a2b8b32ec7c8bdfcee4b91fc
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2023-05-01 20:55:55 -0700

    A working 'hello' script

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -1,21 +1,32 @@
-use tokio::net::{ TcpListener, TcpStream };
-use tokio::io::{ AsyncReadExt, AsyncWriteExt };
+//! Run a Redis server using Docker:
+//!
+//! ```bash
+//! docker run --rm -p 6379:6379 redis:latest
+//! ```
+//!
+//! Then open a TCP connection using netcat:
+//!
+//! ```bash
+//! nc -v 127.0.0.1 6379
+//! ```
+use bytes::BytesMut;
+use tokio::io;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
 
 #[tokio::main]
-async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-
+async fn main() -> io::Result<()> {
+    let mut socket = TcpStream::connect("127.0.0.1:6379").await?;
+    println!("Connected");
     if socket.writable().await.is_ok() {
-        let buffer = "那么古尔丹，代价是什么呢\n".to_string();
-        socket.write_all(buffer.as_bytes()).await.unwrap();
+        let buffer = "*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n"; // "SET foo bar"
+        socket.write_all(&buffer.as_bytes()).await?;
+        println!("Buffer written");
     }
-
-    if socket.readable().await.is_ok() {
-        let mut buffer: Vec<u8> = vec![0;24];
-        socket.read(&mut buffer).await.unwrap();
-
-        let msg = String::from_utf8(buffer).unwrap();
-        println!("{msg}");
-    }
+    socket.readable().await?;
+    let mut buf = BytesMut::new();
+    let nbytes = socket.read_buf(&mut buf).await?;
+    println!("{nbytes} bytes of buffer read");
+    println!("{:?}", buf);
+    return Ok(());
 }
