551fc93e4773cd03e7a55a36e734c8b3b7156acc
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2023-05-02 21:47:17 -0700

    Connection::write_frame tested

--- src/bin/test_connection.rs
+++ src/bin/test_connection.rs
@@ -4,16 +4,16 @@
 //!
 //! This program will send a frame and check if it can read it back
 use bytes::Bytes;
-use tokio::net::TcpStream;
-use tokio::io::AsyncWriteExt;
 use redis::Connection;
 use redis::Frame;
+use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() {
-    let mut socket = TcpStream::connect("127.0.0.1:6379").await.unwrap();
-    let frame = Frame::Bulk(Bytes::from("Hello, world!"));
-    socket.write_all(&frame.serialize()).await.unwrap();
+    let socket = TcpStream::connect("127.0.0.1:6379").await.unwrap();
     let mut conn = Connection::new(socket);
+
+    let frame = Frame::Bulk(Bytes::from("Hello, world!"));
+    conn.write_frame(&frame).await.unwrap();
     assert_eq!(conn.read_frame().await.unwrap(), Some(frame));
 }
